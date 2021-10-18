6fe107b2308ae0512d3dd60c1b1eac44e9b7a769
Bill Konchellah <williamoriginal2019@gmail.com>
2021-10-18 17:59:51 +0300

    "First commit"

---
+++ js/sight.js
@@ -0,0 +1,19 @@
+class SVGElement {
+    constructor(type){
+        this.type = type;
+        this.namespace =  'http://www.w3.org/2000/svg'
+        this.node = document.createElementNS(this.namespace, this.type)
+        return this;
+    }
+
+    attr(attrs){
+        for(const [key, value] of Object.entries(attrs)){
+            this.node.setAttributeNS(null,key,value)
+        }
+        return this
+    }
+
+    append(element){
+        return this
+    }
+}
